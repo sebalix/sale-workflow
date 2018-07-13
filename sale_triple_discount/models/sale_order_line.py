fa2f3a6b4eadec75cad8808d2939fe3589988768
mpanarin <m.panarin@mobilunity.com>
2018-07-13 15:43:11 +0300

    [IMP] 10.0 sale_triple_discount: rework discount computation

--- sale_triple_discount/models/sale_order_line.py
+++ sale_triple_discount/models/sale_order_line.py
@@ -11,22 +11,53 @@ import odoo.addons.decimal_precision as dp
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.depends('discount2', 'discount3')
+    def _get_final_discount(self):
+        self.ensure_one()
+        if self.discounting_type == "additive":
+            return self._additive_discount()
+        elif self.discounting_type == "multiplicative":
+            return self._multiplicative_discount()
+
+    def _additive_discount(self):
+        self.ensure_one()
+        discounts = [getattr(self, x) or 0.0 for x in self._discount_fields()]
+        final_discount = 1 - sum(discounts) / 100
+        if final_discount <= 0:
+            return 0
+        return final_discount
+
+    def _multiplicative_discount(self):
+        self.ensure_one()
+        discounts = [1 - (getattr(self, x) or 0.0) / 100
+                     for x in self._discount_fields()]
+        final_discount = 1
+        for discount in discounts:
+            final_discount *= discount
+        return final_discount
+
+    def _discount_fields(self):
+        return ['discount', 'discount2', 'discount3']
+
+    @api.depends('discount2', 'discount3', 'discounting_type')
     def _compute_amount(self):
+        # updating the values of fields in the compute, that is compute is
+        # depending on can lead to unexpected results, such as recomputation of
+        # the fields once more returning a wrong value
+        super(SaleOrderLine, self)._compute_amount()
         for line in self:
             prev_price_unit = line.price_unit
-            prev_discount = line.discount
-            price_unit = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-            price_unit *= (1 - (line.discount2 or 0.0) / 100.0)
-            price_unit *= (1 - (line.discount3 or 0.0) / 100.0)
+            price = round(prev_price_unit * line._get_final_discount(), 2)
+            taxes = line.tax_id.compute_all(
+                price,
+                line.order_id.currency_id,
+                line.product_uom_qty,
+                product=line.product_id,
+                partner=line.order_id.partner_shipping_id
+            )
             line.update({
-                'price_unit': price_unit,
-                'discount': 0.0,
-            })
-            super(SaleOrderLine, line)._compute_amount()
-            line.update({
-                'price_unit': prev_price_unit,
-                'discount': prev_discount,
+                'price_tax': taxes['total_included'] - taxes['total_excluded'],
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
             })
 
     discount2 = fields.Float(
@@ -38,6 +69,18 @@ class SaleOrderLine(models.Model):
         'Disc. 3 (%)',
         digits=dp.get_precision('Discount'),
         default=0.0,
+    )
+    discounting_type = fields.Selection(
+        string="Discounting type",
+        selection=[
+            ('additive', 'Additive'),
+            ('multiplicative', 'Multiplicative'),
+        ],
+        default="multiplicative",
+        help="Specifies whether discounts should be additive "
+        "or multiplicative.\nAdditive discounts are summed first and "
+        "then applied.\nMultiplicative discounts are applied sequentially.\n"
+        "Multiplicative discounts are default",
     )
 
     _sql_constraints = [
@@ -57,9 +100,9 @@ class SaleOrderLine(models.Model):
 
     @api.depends('discount2', 'discount3')
     def _get_price_reduce(self):
+        # with introduction of additive discounts we need to change the way
+        # discount is computed
+        super(SaleOrderLine, self)._get_price_reduce()
         for line in self:
-            super(SaleOrderLine, line)._get_price_reduce()
-            if line.discount2:
-                line.price_reduce *= (1 - line.discount2 / 100)
-            if line.discount3:
-                line.price_reduce *= (1 - line.discount3 / 100)
+                line.price_reduce = (line.price_unit *
+                                     line._get_final_discount())
